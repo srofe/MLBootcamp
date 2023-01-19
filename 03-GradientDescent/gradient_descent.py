c194189bb70e6289e1aa0874664c158c0d785683
Simon Rofe <simon.rofe@ieee.org>
2023-01-19 17:45:09 +1000

    Show how gradient descent can diverge
    Add parameter to the gradient_descent() function to control the number
    of iteration steps to be taken. This allows us to control when the
    algorithm reaches the limits of floating point numbers as it diverges
    towards infinity.

--- 03-GradientDescent/gradient_descent.py
+++ 03-GradientDescent/gradient_descent.py
@@ -1,9 +1,9 @@
-def gradient_descent(derivative_function, initial_guess, multiplier=0.02, precision=0.001) :
+def gradient_descent(derivative_function, initial_guess, multiplier=0.02, precision=0.001, maximum_iterations=500):
     new_x = initial_guess
     x_list = []
     slope_list = []
 
-    for n in range(500) :
+    for n in range(maximum_iterations):
         previous_x = new_x
         gradient = derivative_function(previous_x)
         new_x = previous_x - multiplier * gradient
@@ -13,7 +13,7 @@ def gradient_descent(derivative_function, initial_guess, multiplier=0.02, precis
         x_list.append(new_x)
         slope_list.append(derivative_function(new_x))
 
-        if step_size < precision :
+        if step_size < precision:
             break
 
     return new_x, x_list, slope_list
